bf2060ee4e90f7063c1d62c9a3f3f064d1ce3da5
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-07 18:31:09 +0900

    Added lomboc dependency and DTO test code

--- src/main/java/com/jongyun/book/springboot/web/HelloController.java
+++ src/main/java/com/jongyun/book/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.jongyun.book.springboot.web;
 
+import com.jongyun.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /*
@@ -13,7 +15,14 @@ public class HelloController {
     * HTTP Method 인 Get 요청을 받을 수 있는 API 를 만들어 줍니다.
      */
     @GetMapping("/hello")
-    public String hello() {
+    public String helloDto() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(
+            @RequestParam("name") String name,
+            @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/jongyun/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jongyun/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
 // RunWith : 테스트를 진행할 때 Junit 에 내장된 실행자 외에 다른 실행자를 실행 시킵니다.
@@ -33,5 +35,19 @@ public class HelloControllerTest {
         perform
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
+
+    @Test
+    public void returnHelloDTO() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
