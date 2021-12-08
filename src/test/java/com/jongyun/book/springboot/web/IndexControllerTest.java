40d029c09a00c252070b9870a0cdf9c4ace581ed
Jongyun Ha <70758906+JongyunHa@users.noreply.github.com>
2021-12-08 15:59:26 +0900

    Added Template engine mustache and create index page

---
+++ src/test/java/com/jongyun/book/springboot/web/IndexControllerTest.java
@@ -0,0 +1,30 @@
+package com.jongyun.book.springboot.web;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void loadingIndexPage() {
+        //when
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        //then
+        Assertions.assertThat(body).contains("Web service starting with spring boot");
+    }
+
+}
